476a0168cc36a07784238e42ed35f7bfd4744d4f
sakulstra <lukasstrassel@googlemail.com>
2020-12-06 00:25:41 +0100

    fix: try cheap trick

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import * as v1 from './v1';
+import * as v2 from './v2';
+
 export * from './helpers/bignumber';
 export { BigNumber } from 'bignumber.js';
 export * from './helpers/pool-math';
+
+export * from './v2';
+export { v1, v2 };
