f0562b8f94c49e912dacf7ddc77a7f345a2794d3
sakulstra <lukasstrassel@googlemail.com>
2021-02-05 18:27:54 +0100

    test: add another test

--- src/test/ray-math.test.ts
+++ src/test/ray-math.test.ts
@@ -1,12 +1,13 @@
 import BigNumber from 'bignumber.js';
 import { SECONDS_PER_YEAR } from '../helpers/constants';
-import { valueToZDBigNumber } from '../helpers/bignumber';
+import { BigNumberValue, valueToZDBigNumber } from '../helpers/bignumber';
 import {
   RAY,
   rayMul,
   rayPow,
   binomialApproximatedRayPow,
 } from '../helpers/ray-math';
+import { calculateCompoundedInterest, normalize } from '../helpers/pool-math';
 
 describe('wadMul should', () => {
   it('works correct', () => {
@@ -20,7 +21,17 @@ describe('wadMul should', () => {
   });
 });
 
-describe('rayPow', () => {
+const legacyCalculateCompoundedInterest = (
+  rate: BigNumberValue,
+  currentTimestamp: number,
+  lastUpdateTimestamp: number
+): BigNumber => {
+  const timeDelta = valueToZDBigNumber(currentTimestamp - lastUpdateTimestamp);
+  const ratePerSecond = valueToZDBigNumber(rate).dividedBy(SECONDS_PER_YEAR);
+  return rayPow(ratePerSecond.plus(RAY), timeDelta);
+};
+
+describe('rayPow and binomialApproximatedRayPow', () => {
   it('should be roughly equal', () => {
     const result = rayPow(
       valueToZDBigNumber('323788616402133497883602337')
@@ -35,5 +46,38 @@ describe('rayPow', () => {
       valueToZDBigNumber(60 * 60 * 24)
     ).toString();
     expect(result.substring(0, 8)).toEqual(approx.substring(0, 8));
+  });
+
+  it('should not be far off for big amounts and time spans', () => {
+    /**
+     * We calculate the balance based on the last user interaction.
+     * This means, while it's very unlikely if someone deposited 100M and didn't touch it for 5years with and interest of 10% the error should be small.
+     */
+    const rate = valueToZDBigNumber('10000000000000000000000000'); // 10%
+    const balance = '100000000000000000000000000'; // 100M ETH
+    const accurateInterest = legacyCalculateCompoundedInterest(
+      rate,
+      60 * 60 * 24 * 365 * 5,
+      0
+    ).toString();
+    const approximatedInterest = calculateCompoundedInterest(
+      rate,
+      valueToZDBigNumber(60 * 60 * 24 * 365 * 5),
+      valueToZDBigNumber(0)
+    ).toString();
+    expect(accurateInterest.substr(0, 5)).toEqual(
+      approximatedInterest.substr(0, 5)
+    );
+    const accurateBalance = normalize(rayMul(accurateInterest, balance), 18);
+    const approximatedBalance = normalize(
+      rayMul(approximatedInterest, balance),
+      18
+    );
+    expect(
+      Math.abs(
+        Number.parseFloat(accurateBalance) -
+          Number.parseFloat(approximatedBalance)
+      )
+    ).toBeLessThan(10000);
   });
 });
