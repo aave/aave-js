9aa816b1313dea51d448c2fbd33f50fb989f23a9
sakulstra <lukasstrassel@googlemail.com>
2021-02-08 19:50:46 +0100

    fix: speedup normalize

--- src/helpers/bignumber.ts
+++ src/helpers/bignumber.ts
@@ -17,18 +17,20 @@ export function valueToZDBigNumber(amount: BigNumberValue): BigNumber {
 
 const bn10 = new BigNumber(10);
 
-const bn10PowLookup: { [key: number]: BigNumberValue } = {};
+const bn10PowLookup: { [key: number]: BigNumber } = {};
 
 /**
  * It's a performance optimized version of 10 ** x, which essentially memoizes previously used pows and resolves them as lookup.
  * @param decimals
  * @returns 10 ** decimals
  */
-export function pow10(decimals: number) {
+export function pow10(decimals: number): BigNumber {
   if (!bn10PowLookup[decimals]) bn10PowLookup[decimals] = bn10.pow(decimals);
   return bn10PowLookup[decimals];
 }
 
 export function normalize(n: BigNumberValue, decimals: number): string {
-  return new BigNumber(n).dividedBy(pow10(decimals)).toString(10);
+  return valueToBigNumber(n)
+    .dividedBy(pow10(decimals))
+    .toString(10);
 }
