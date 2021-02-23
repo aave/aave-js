5c3d72d5f3f520ccae6abe799706c28a13881986
sendra <polsendra@gmail.com>
2021-02-23 10:56:08 +0100

    fix: add new matic market

--- src/tx-builder/config/index.ts
+++ src/tx-builder/config/index.ts
@@ -106,7 +106,8 @@ export const enabledNetworksByService: EnabledNetworksType = {
     [Stake.Aave]: [Network.kovan, Network.mainnet],
   },
   lendingPool: {
-    [Market.Proto]: [Network.kovan, Network.mainnet, Network.matic],
+    [Market.Proto]: [Network.kovan, Network.mainnet],
+    [Market.Matic]: [Network.matic],
   },
   governance: [Network.kovan, Network.mainnet],
   wethGateway: [Network.kovan, Network.mainnet, Network.matic],
