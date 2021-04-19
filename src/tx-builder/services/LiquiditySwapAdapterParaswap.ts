f0cd194e74ac59e16e0fd2fd501028e9fdfb476e
sakulstra <lukasstrassel@googlemail.com>
2021-04-19 19:54:24 +0200

    fix: updadte offsets

--- src/tx-builder/services/LiquiditySwapAdapterParaswap.ts
+++ src/tx-builder/services/LiquiditySwapAdapterParaswap.ts
@@ -19,6 +19,14 @@ export function augustusFromAmountOffsetFromCalldata(calldata: string) {
   switch (calldata.slice(0, 10)) {
     case '0xda8567c8': // Augustus V3 multiSwap
       return 4 + 32 + 2 * 32;
+    case '0x58b9d179': // Augustus V4 swapOnUniswap
+      return 4;
+    case '0x0863b7ac': // Augustus V4 swapOnUniswapFork
+      return 4 + 2 * 32;
+    case '0x8f00eccb': // Augustus V4 multiSwap
+      return 4 + 32 + 32;
+    case '0xec1d21dd': // Augustus V4 megaSwap
+      return 4 + 32 + 32;
     default:
       throw new Error('Unrecognized function selector for Augustus');
   }
